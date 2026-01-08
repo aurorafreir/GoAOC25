d2a6387d1fe521ae7c7aeee9030e6e530af8d81e
aurorafreir <aurorafreir@gmail.com>
2026-01-08 23:40:56 +0000

    Fixing and commenting d01p2 wraparound code

--- 01/01.go
+++ 01/01.go
@@ -74,15 +74,19 @@ func partB() int {
 		}
 
 		// Handle wraparound
-		numRotations := 0
-		negative := 0
+		numRotations := 0 // Number of times that the currentVal is wrapped past 100
+		polarity := 1     // 1 if $currentVal is positive, -1 is $currentVal is negative
 		if currentVal >= 100 || currentVal < 0 {
+			// Gets the specific number of times that currentVal is wrapped past 100
 			numRotations = intFloor(generics.AbsInt(currentVal) / 100)
-			if currentVal < 0 {
-				negative = 1
-			}
+			// Gets the Absolute Int number of rotations
 			numRotations = max(generics.AbsInt(numRotations), 1)
-			currentVal += (100 * numRotations) * negative
+			// Sets $negative to -1 if the $currentVal is below zero
+			if currentVal < 0 {
+				polarity = -1
+			}
+			// Subtracts 100 * the number of rotations * the polarity (negative/positive)
+			currentVal -= (100 * numRotations) * polarity
 		}
 		fmt.Println(currentVal, numRotations)
 
