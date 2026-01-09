07f1d988fcb9c6a4c6d8abbfa9bfe0087ab3806a
aurorafreir <aurorafreir@gmail.com>
2026-01-09 20:18:34 +0000

    Handling of start/end ranges as ints for each input range

--- 02/02.go
+++ 02/02.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"example.com/generics"
 )
 
 func partA() int {
+	// Basic file importing
 	ex, err := os.Getwd()
 	generics.Check(err)
 
@@ -16,9 +19,28 @@ func partA() int {
 	data, err := generics.ReadLines(path)
 	generics.Check(err)
 
+	// Get just the first line as a string
 	singleString := data[0]
-	fmt.Println(singleString)
+	// fmt.Println(singleString)
 
+	// Split the input string by "," into each inputRange
+	inputRanges := strings.Split(singleString, ",")
+	for _, inputRange := range inputRanges {
+		// Get individual Start and End ranges as strings
+		startRange := strings.Split(inputRange, "-")[0]
+		endRange := strings.Split(inputRange, "-")[1]
+
+		// Convert Start and End strings into ints
+		startRangeInt, err := strconv.Atoi(startRange)
+		generics.Check(err)
+		endRangeInt, err := strconv.Atoi(endRange)
+		generics.Check(err)
+
+		fmt.Println("start:", startRangeInt, "end:", endRangeInt)
+		for i := startRangeInt; i <= endRangeInt; i++ {
+			fmt.Println(i)
+		}
+	}
 	return 0
 }
 
