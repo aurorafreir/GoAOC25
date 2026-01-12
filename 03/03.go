86c7436be38aaa071a47db1cbe06d6a12627f362
aurorafreir <aurorafreir@gmail.com>
2026-01-12 21:19:26 +0000

    Day 3 part 1 working

--- 03/03.go
+++ 03/03.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"example.com/utils"
 )
 
 func partA() int {
+	var maxInts []int
+	joltage := 0 // i like this word
+
 	// Basic file importing
 	ex, err := os.Getwd()
 	utils.Check(err)
@@ -16,11 +21,37 @@ func partA() int {
 	path := filepath.Join(ex, "input.txt")
 	data, err := utils.ReadLines(path)
 	utils.Check(err)
-	for _, line := range data {
 
+	for _, line := range data {
+		lenOfNum := len(line)
+		maxInt := 0
+		// Loop through each possible forward-only iteration of the input line
+		for x := 0; x <= lenOfNum; x++ {
+			for y := x; y < lenOfNum; y++ {
+				if y == x {
+					continue
+				}
+				// Concatenate item $X and item $Y as a string
+				var b bytes.Buffer
+				b.WriteString(line[x : x+1])
+				b.WriteString(line[y : y+1])
+				// Convert to integer
+				asInt, err := strconv.Atoi(b.String())
+				utils.Check(err)
+				// Replace $maxInt if $asInt is higher
+				if asInt > maxInt {
+					maxInt = asInt
+				}
+			}
+		}
+		maxInts = append(maxInts, maxInt)
 	}
 
-	return 0
+	// Addition of each int in $maxInts
+	for _, int := range maxInts {
+		joltage += int
+	}
+	return joltage
 }
 
 func partB() int {
