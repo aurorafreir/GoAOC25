5aa3767ae1662200eaed06436595989d8f215bbf
aurorafreir <aurorafreir@gmail.com>
2026-01-26 23:22:18 +0000

    d5p2 WIP, not working yet, but clean ranges

--- 05.go
+++ 05.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -58,21 +59,50 @@ func d5p2() (int, error) {
 	data, err := utils.AOCFileReadToSlice(true, 5)
 	utils.Check(err)
 
-	ranges, _ := returnRangesAndIDs(data, true)
-
-	sortedRanges := make(map[int][][]int)
-
-	cleanRanges := make([][]int, 0)
+	// [x] Get ranges
+	// [x] Get a slice of just the input range numbers
+	// [x] Sort input range sort
+	// [x] Create map of input ranges, key being start range and value being output range
+	// [x] Loop through sorted slice to grab k,v from map and export a sorted input/output slice
+	// [x] loop through sorted slice, and given it's output range, see if the next input range is
+	// 			lower than the current output range, if it is, append [$current[input], $next[ouxtput]]
+	// [ ] Recursively run over sorted slices to weed out overlapping items
 
 	count := 0
+	var inRanges []int
+	inOutRangesMap := make(map[int]int)
+	sortedRanges := [][]int{}
 
-	for index, startEndRange := range ranges[:len(ranges)-1] {
-		if startEndRange[1] >= ranges[index+1][0] {
-			fmt.Println(startEndRange, ranges[index+1][0])
-		}
+	ranges, _ := returnRangesAndIDs(data, true)
+
+	for _, startEndRange := range ranges {
+		inOutRangesMap[startEndRange[0]] = startEndRange[1]
 	}
 
-	fmt.Println(ranges, cleanRanges)
+	for _, startEndRange := range ranges {
+		inRanges = append(inRanges, startEndRange[0])
+	}
+
+	sort.Ints(inRanges) // this does it in place?? weird.
+
+	for _, item := range inRanges {
+		newItem := []int{item, inOutRangesMap[item]}
+		sortedRanges = append(sortedRanges, newItem)
+	}
+
+	fmt.Println(inOutRangesMap)
+	fmt.Println(inRanges)
+	fmt.Println(sortedRanges)
+
+	// rangesToMap := make[]
+
+	// for index, startEndRange := range ranges[:len(ranges)-1] {
+	// 	if startEndRange[1] >= ranges[index+1][0] {
+	// 		fmt.Println(startEndRange, ranges[index+1][0])
+	// 	}
+	// }
+
+	// fmt.Println(ranges, cleanRanges)
 
 	return count, nil
 }
