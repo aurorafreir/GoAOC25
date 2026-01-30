e63255d38ba996e7a012c8ac65053fce9be2518b
Aurora Freir <aurorafreir@gmail.com>
2026-01-30 18:52:01 +0000

    Day 2 part 1 working

--- 02.go
+++ 02.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strconv"
 	"strings"
 
@@ -15,64 +14,39 @@ func lenItoa(i int) int {
 }
 
 func d2p1() (output int, err error) {
-	data, err := utils.AOCFileReadToSlice(true, 2)
+	data, err := utils.AOCFileReadToSlice(false, 2)
 	utils.Check(err)
 
 	// Get just the first line as a string
 	singleString := data[0]
 
-	// Split the input string by "," into each inputRange
-	inputRanges := strings.Split(singleString, ",")
-	for _, inputRange := range inputRanges {
-		// Get individual Start and End ranges as strings
-		startRange := strings.Split(inputRange, "-")[0]
-		endRange := strings.Split(inputRange, "-")[1]
+	sliceOfRangeStrings := strings.Split(singleString, ",") // Split single string into each range as a string
+	sliceOfRangeInts := []utils.MinMaxRange{}
 
-		// Convert Start and End strings into ints
-		startRangeInt, err := strconv.Atoi(startRange)
-		utils.Check(err)
-		endRangeInt, err := strconv.Atoi(endRange)
-		utils.Check(err)
+	for _, item := range sliceOfRangeStrings {
+		// Get each range as a slice of two strings, then convert to ints,
+		// 		then append to $sliceOfRangeInts as a $MinMaxRange struc
+		ranges := strings.Split(item, "-")
+		rangeMin, _ := strconv.Atoi(ranges[0])
+		rangeMin = max(rangeMin, 10) // No point in checking integers under 10
+		rangeMax, _ := strconv.Atoi(ranges[1])
+		sliceOfRangeInts = append(sliceOfRangeInts, utils.MinMaxRange{rangeMin, rangeMax})
+	}
 
-		// Get start and end of ranges and loop through them
-		fmt.Println("start:", startRangeInt, "end:", endRangeInt)
-		// lenOfEndInt := lenItoa(endRangeInt)
-		// fmt.Println(lenOfEndInt)
-		// for i := startRangeInt; i <= endRangeInt; i++ {
+	invalidCount := 0
 
-		for i := startRangeInt; i <= endRangeInt; i++ {
-			// Find looping numbers in each int
-			// fmt.Println(i)
-			lenOfInt := lenItoa(i)
-			// fmt.Println(lenOfInt)
-			var sliceOfStrs []string
-
-			// Splits each integer into a Slice of ints of each
-			for x := 0; x <= lenOfInt; x++ {
-				for y := x; y < lenOfInt; y++ {
-					if y == x {
-						continue
-					}
-					slicedIntAsStr := strconv.Itoa(i)
-					slicedStr := slicedIntAsStr[x:y]
-					utils.Check(err)
-					// No point in saving the single length items
-					if len(slicedStr) > 1 {
-						sliceOfStrs = append(sliceOfStrs, slicedStr)
-					}
-				}
+	// Find all the invalid items in the ranges
+	for _, ranges := range sliceOfRangeInts {
+		for number := ranges.Min; number <= ranges.Max; number++ {
+			numberAsStr := strconv.Itoa(number)
+			halfLength := len(numberAsStr) / 2
+			if numberAsStr[0:halfLength] == numberAsStr[halfLength:] { // Split the item into two same length items and see if they match
+				invalidCount += number
 			}
-			// Sorts, and then removes duplicates from $sliceOfStrs, outputs to $sortedSliceOfStrs
-			slices.Sort(sliceOfStrs)
-			sortedSliceOfStrs := slices.Compact(sliceOfStrs)
-			fmt.Println(sortedSliceOfStrs)
-			// regexComp := strings.Join(sortedSliceOfStrs, "|")
-			// regexComp := regexp.MustCompile(strings.Join(sortedSliceOfStrs, "|"))
-			// fmt.Println(regexComp)
-			// fmt.Println(len(regexComp.FindAllString(strconv.Itoa(i), -1)))
 		}
 	}
-	return 0, nil
+
+	return invalidCount, nil
 }
 
 func d2p2() (output int, err error) {
